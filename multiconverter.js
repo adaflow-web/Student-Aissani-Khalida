379ef232822492160676cfd0be356860c5b4acd3
Khalida <aissanikhalida1@gmail.com>
2022-12-20 23:04:30 +0100

    improve the code!

--- multiconverter.js
+++ multiconverter.js
@@ -1,53 +1,56 @@
-var answerIsCorrect = true ;
-var UserInput = prompt("Do you want to convert a value? (yes / No)");
+
 
 function convertOne (euro) {
     
-    var message = ("the result is ");
     var result = euro * 1.082;
-    return message + result.toString();
+    return "the result is " + result.toString();
 }
 
 function convertTwo (celsius){
 
-    var message = ("the result is ");
     var result = (celsius * 9/5) + 32;
-    return message + result.toString();
+    return "the result is " + result.toString();
 }
 
 function convertThree (liters){
-    var message = ("the result is ");
+    
     var result = liters * 33.814;
-    return message + result.toString();
+    return "the result is " + result.toString();
 
 }
 
+//var answerIsCorrect = true ;
+var UserInput = prompt("Do you want to convert a value? (yes / No)");
 
-if (answerIsCorrect){
+
+while (UserInput == "yes"){
     
     var UserInputOne = prompt("Which Conversion? chf/Fahrenheit/OZ");
 
+
+
     if (UserInputOne == "chf") {
-        var UserInputTwo = prompt("Which value do you to convert?");
+        var UserInputTwo = prompt("Enter a value?");
         result = convertOne (UserInputTwo);
         console.log (result);
     
     } else if (UserInputOne == "Fahrenheit") {
-        var UserInputTwo = prompt("Which value do you to convert?");
+        var UserInputTwo = prompt("Enter a value?");
         result = convertTwo (UserInputTwo);
         console.log (result);
     
     } else if (UserInputOne == "OZ") {
-        var UserInputTwo = prompt("Which value do you to convert?");
+        var UserInputTwo = prompt("Enter a value?");
         result = convertThree (UserInputTwo);
         console.log (result);
 
+      
 
-    } else {
+    }else {
 
         console.log("The conversion is not supported!");
     }
-
+     
     
 } 
 
