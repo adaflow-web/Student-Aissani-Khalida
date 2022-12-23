764e05372b7da53cb737495e453f761686e96ed2
Aissani1977 <88311791+Aissani1977@users.noreply.github.com>
2022-12-23 10:28:54 +0100

    Delete secretword.js

--- secretword.js
+++
@@ -1,9 +0,0 @@
-// This function takes a boolean parameter
-// that says if password was entered correctly
-// (true = yes, false = no)
-function logIn(passwordIsCorrect) {
-    if (passwordIsCorrect != true) {
-      console.log("Your password is not correct, you can't log in");
-    }
-    console.log("Welcome back! You are now signed in.");
-  }
