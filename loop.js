039afe7a43b886feba85792ab45a33dfd692e182
Aissani1977 <88311791+Aissani1977@users.noreply.github.com>
2022-12-23 10:22:05 +0100

    Delete loop.js

--- loop.js
+++
@@ -1,8 +0,0 @@
-var myFavoriteDishes = ["couscous", "rechta", "berkoukes"];
-console.log ("Here are my_favorite_dishes");
-
-for (i = 0; i < myFavoriteDishes.length; i = i + 1){
-  console.log(myFavoriteDishes[i]);
-}
-console.log ("what are your favorite dishes?");
-
